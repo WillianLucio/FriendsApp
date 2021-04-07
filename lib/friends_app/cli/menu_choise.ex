c201c5de9f00df1524afea6438a686fb0f86f3f4
willian lucio <willian.l.lucio@gmail.com>
2021-04-07 20:11:01 -0300

    Use mix format.

--- lib/friends_app/cli/menu_choise.ex
+++ lib/friends_app/cli/menu_choise.ex
@@ -4,16 +4,16 @@ defmodule FriendsApp.CLI.MenuChoise do
   def start do
     Shell.cmd("clear")
     Shell.info("Escolha uma opção:")
-    
+
     FriendsApp.CLI.MenuItems.all()
-    |> Enum.map(&(&1.label))
+    |> Enum.map(& &1.label)
     |> display_options
   end
 
   defp display_options(options) do
     options
     |> Enum.with_index(1)
-    |> Enum.each(fn {option, index} -> 
+    |> Enum.each(fn {option, index} ->
       Shell.info("#{index} - #{option}")
     end)
   end
--- lib/friends_app/cli/menu_items.ex
+++ lib/friends_app/cli/menu_items.ex
@@ -1,10 +1,11 @@
 defmodule FriendsApp.CLI.MenuItems do
   alias FriendsApp.CLI.Menu
 
-  def all, do: [
-    %Menu{ label: "Cadastrar um amigo", id: :create },
-    %Menu{ label: "Lista os amigos",    id: :read },
-    %Menu{ label: "Atualiza um amigo",  id: :update },
-    %Menu{ label: "Deleta um amigo",    id: :delete },
-  ]
+  def all,
+    do: [
+      %Menu{label: "Cadastrar um amigo", id: :create},
+      %Menu{label: "Lista os amigos", id: :read},
+      %Menu{label: "Atualiza um amigo", id: :update},
+      %Menu{label: "Deleta um amigo", id: :delete}
+    ]
 end
