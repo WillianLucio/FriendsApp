f47d0fdb6daab8a544354ed5dbc0f8625e96e205
willian lucio <willian.l.lucio@gmail.com>
2021-04-07 19:49:04 -0300

    Lesson 50 - Adjust initialization messages.

--- lib/mix/taks/start.ex
+++ lib/mix/taks/start.ex
@@ -2,7 +2,5 @@ defmodule Mix.Tasks.Start do
   use Mix.Task
 
   @shortdoc "Start [Friends App]"
-  def run(_) do
-    IO.puts("\n\nIniciando a aplicação!!!\n\n")
-  end
+  def run(_), do: FriendsApp.CLI.Main.start_app()
 end
